ac505041ac27f4cdad24f0beee93ac77102bd1f4
daniel-vazquez <da.vazquez728@gmail.com>
2021-05-24 21:08:22 -0700

    testing api with jest

---
+++ __test__/auth.test.js
@@ -0,0 +1,50 @@
+const supertest = require("supertest")
+const server = require("../api/server")
+const db = require("../data/db")
+
+describe("plants integration tests", () => {
+  it("return error message for plants endpoint when user not authenticated", async () => {
+    const res = await supertest(server).get("/api/plants")
+    expect(res.statusCode).toBe(401)
+    expect(res.type).toBe("application/json")
+    expect(res.body.message).toBe("Token required")
+  })
+})
+describe("registration integration tests", () => {
+  it("should allow a new user to register", async () => {
+    const res = await supertest(server)
+      .post("/api/auth/register")
+      .send({username: "json statham", password: "hello", phone: "123456789"})
+    expect(res.statusCode).toBe(200)
+    expect(res.type).toBe("application/json")
+    expect(res.body.username).toBe("json statham")
+    expect(res.body.id).toBeDefined()
+  })
+}) 
+it("should not allow a user to register without a password", async () => {
+  const res = await supertest(server)
+    .post("/api/auth/register")
+    .send({username: "bobby", password: "", phone: ""})
+  expect(res.statusCode).toBe(401)
+  expect(res.type).toBe("application/json")
+  expect(res.body.message).toBe("Username and Password required or incorrect values")
+  expect(res.body.id).not.toBeDefined()
+})
+describe("login integration tests", () => {
+  it("should allow an existing user to login", async () => {
+    const res = await supertest(server)
+      .post("/api/auth/login")
+      .send({username: "test", password: "test"})
+    expect(res.statusCode).toBe(200)
+    expect(res.type).toBe("application/json")
+    expect(res.body.message).toContain("Welcome back test")
+  })
+  it("should not allow an existing user to login with incorrect password", async () => {
+    const res = await supertest(server)
+      .post("/api/auth/login")
+      .send({username: "test", password: "test123"})
+    expect(res.statusCode).toBe(401)
+    expect(res.type).toBe("application/json")
+    expect(res.body.message).toContain("invalid credentials")
+  })
+})
